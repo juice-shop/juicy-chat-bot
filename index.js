817f757542a01479428aaea16fa3ae86b433e1ef
Scar26 <mmatty26@gmail.com>
2020-05-21 15:24:15 +0530

    Add file endings
    Signed-off-by: Scar26 <mmatty26@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { VM } = require('vm2');
 const fs = require('fs');
 const ctx = fs.readFileSync('./factory.js').toString();
+
 class Bot {
     constructor(name, greeting, training_set) {
         this.name = name;
