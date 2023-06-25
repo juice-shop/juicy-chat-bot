147fb1627154fd3a2eb263623f0ca59bdb31d443
Jannik Hollenbach <jannik@hollenbach.de>
2023-06-25 07:10:49 +0200

    Only load english (#15)
    * Only load english
    
    This improved the start up time quite a bit.
    A lot of time is currently spend loading all the locals.
    
    Other languages are not supported by juice-shop, probably not worth
    having them supported by juicy-chat-bot.
    
    * Auto-fix linting issues
    
    Signed-off-by: JuiceShopBot <61591748+JuiceShopBot@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: JuiceShopBot <61591748+JuiceShopBot@users.noreply.github.com>
    Co-authored-by: JuiceShopBot <61591748+JuiceShopBot@users.noreply.github.com>

--- nlp/sentiment-analyzer.js
+++ nlp/sentiment-analyzer.js
@@ -24,13 +24,13 @@
 const {
   SentimentAnalyzer: SentimentAnalyzerBase
 } = require('@nlpjs/sentiment')
-const { LangAll } = require('@nlpjs/lang-all')
+const { LangEn } = require('@nlpjs/lang-en')
 const { Nlu } = require('@nlpjs/nlu')
 
 class SentimentAnalyzer extends SentimentAnalyzerBase {
   constructor (settings = {}, container) {
     super(settings, container)
-    this.container.use(LangAll)
+    this.container.use(LangEn)
     this.container.use(Nlu)
   }
 
