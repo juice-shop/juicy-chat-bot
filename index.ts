fd039bdd4bbec2c41f13f195245c92baa735728d
Harsh Kumar <harshmastic@gmail.com>
2025-08-25 06:06:21 +0530

    Add Bot class for chatbot functionality with NLP integration

---
+++ index.ts
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2020-2023 Bjoern Kimminich & the OWASP Juice Shop contributors.
+ * SPDX-License-Identifier: MIT
+ */
+
+import { VM } from 'vm2'
+import fs from 'fs'
+import path from 'path'
+import NlpManager from './nlp'// eslint-disable-line no-unused-vars
+const ctx = fs.readFileSync(`${__dirname}/factory.js`).toString()
+
+interface Training {
+  state: boolean
+  data: string
+}
+
+interface BotResponse {
+  action: string
+  body: string
+}
+
+interface BotQueryResponse {
+  action: string
+  body?: string
+  [key: string]: any
+}
+
+class Bot {
+  name: string
+  greeting: string
+  defaultResponse: BotResponse
+  training: Training
+  factory: any
+
+  constructor (
+    name: string,
+    greeting: string,
+    trainingSet: string,
+    defaultResponse: string
+  ) {
+    this.name = name
+    this.greeting = greeting
+    this.defaultResponse = { action: 'response', body: defaultResponse } as BotResponse
+    this.training = {
+      state: false,
+      data: trainingSet
+    } as Training
+    this.factory = new VM({
+      sandbox: {
+        Nlp: NlpManager,
+        training: this.training
+      }
+    })
+    this.factory.run(ctx)
+    this.factory.run(`trainingSet=${trainingSet}`)
+  }
+
+  greet (token: string): string {
+    return this.render(this.greeting, token)
+  }
+
+  render (statement: string, token: string): string {
+    return statement.replace(/<bot-name>/g, this.name).replace(/<customer-name>/g, this.factory.run(`currentUser(${token})`))
+  }
+
+  addUser (token: string, name: string): void {
+    this.factory.run(`users.addUser("${token}", "${name}")`)
+  }
+
+  getUser (token: string): string {
+    return this.factory.run(`users.get("${token}")`)
+  }
+
+  async respond (query: string, token: string): Promise<BotResponse> {
+    const response: BotQueryResponse = (await this.factory.run(`process("${query}", "${token}")`)).answer
+    if (!response) {
+      return this.defaultResponse
+    } else {
+      if (response.body) {
+        response.body = this.render(response.body, token)
+      }
+      return response as BotResponse
+    }
+  }
+
+  train () {
+    return this.factory.run('train()')
+  }
+}
+
+export default Bot
